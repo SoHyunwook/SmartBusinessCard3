dd44ee11101165ba1dcf2d1007c6836926677645
SoHyunwook <hyunwook1994@naver.com>
2016-11-09 17:53:49 +0900

    .

---
+++ app/src/main/java/com/example/smartbusinesscard2/SplashScreen.java
@@ -0,0 +1,104 @@
+package com.example.smartbusinesscard2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by 현욱 on 2016-11-09.
+ */
+public class SplashScreen extends Activity {
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    TextView textView;
+    Thread splashTread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        ImageView gyroView = (ImageView) findViewById(R.id.gyro);
+        gyroView.setBackgroundResource(R.drawable.gyro_animation);
+        AnimationDrawable gyroAnimation = (AnimationDrawable) gyroView.getBackground();
+
+        gyroAnimation.start();
+        StartAnimations();
+
+        textView = (TextView) findViewById(R.id.text);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                    textView.startAnimation(animation);
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fade);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.gyro);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 3500) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(SplashScreen.this,
+                            MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashScreen.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashScreen.this.finish();
+                }
+            }
+        };
+        splashTread.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+
+    }
+
+}
